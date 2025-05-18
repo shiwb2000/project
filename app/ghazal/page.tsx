c1a87c7ee681ab669c183950a70e364446e6055e
HosseinAzadpour <125812276+HosseinAzadpour@users.noreply.github.com>
2025-05-18 21:10:40 +0330

    1

--- app/ghazal/page.tsx
+++ app/ghazal/page.tsx
@@ -1,3 +1,15 @@
+import Title1 from "./components/Title1";
+import Title2 from "./components/Title2";
+import Title3 from "./components/Title3";
+import Title from "./components/Title";
 export default function Home() {
-  return <div>ghazal page</div>;
+  const name = "ghazal";
+  return (
+    <main>
+      <Title props={name} />
+      <Title props='Title1' />
+      <Title props='Title2' />
+      <Title props='Title3' />
+    </main>
+  );
 }
