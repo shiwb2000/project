e8696568ccab3603dde6f97d05fcdb0f4971c5d1
HosseinAzadpour <125812276+HosseinAzadpour@users.noreply.github.com>
2025-05-21 21:00:59 +0330

    2

---
+++ app/function/script.js
@@ -0,0 +1,25 @@
+export async function getWeather() {
+  console.log("hi");
+  //   const city = document.getElementById("cityInput").value.trim();
+  //   const apiKey = "6ec330f40130ee16fad4deaa9ff60830"; // API کلید
+  //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fa`;
+  //   const weatherDiv = document.getElementById("weather");
+  //   if (!city) {
+  //     weatherDiv.textContent = "لطفا نام یک شهر را وارد کنید";
+  //     return;
+  //   }
+  //   try {
+  //     const response = await fetch(url);
+  //     if (!response.ok) {
+  //       weatherDiv.textContent = "شهر مورد نظر یافت نشد";
+  //       return;
+  //     }
+  //     const data = await response.json();
+  //     const temp = data.main.temp;
+  //     const description = data.weather[0].description;
+  //     weatherDiv.textContent = `دمای شهر ${city} هم اکنون ${temp} درجه سانتی گراد است و وضعیت هوا ${description} است.`;
+  //   } catch (error) {
+  //     weatherDiv.textContent = "خطا در دریافت اطلاعات";
+  //     console.error(error);
+  //   }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import { getWeather } from "./function/script";
+import "./style/style.css";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>برنامه نمایش دمای هوا</h1>
+      <input
+        type='text'
+        id='cityInput'
+        placeholder='نام شهر خود را وارد کنید'
+      />
+      <button onClick={() => getWeather()}>نمایش شهر</button>
+      <div id='weather'></div>
+    </div>
+  );
 }
